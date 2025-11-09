c813dfda205841a98e8ae322761aceb9652084fe
Gokuldas Shenoy.K <74420066+Gokuldas-dev@users.noreply.github.com>
2025-11-09 11:36:19 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+# This is the [PROGRAM FOR Python-Slot-Machine]
+#Global varibale go here --
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
+
+def deposit(): # this is a function to check the amount is digit or not and to make it as a digit (integer)
+  while True:
+    amount = input("what would you like to deposit : ")
+    if amount.isdigit():
+      amount = int(amount)
+      if amount > 0 :
+        break
+      else:
+        print ("The amount must be greater than zero ! ")  # this should be greater than zero 
+    else:
+      print ("Please enter a number :") 
+
+  return amount  #if true the amount will be submited and will be a digit.
+
+def get_number_of_lines():
+  while True:
+    lines = input("Enter the number of lines to bet on  (1-" + str(MAX_LINES) + ")" )
+    if lines.isdigit():
+      lines = int(lines)
+      if 1<= lines <= MAX_LINES:
+        break
+      else:
+        print ("Enter the valid number of lines :  ")  # this should be greater than zero 
+    else:
+      print ("Please enter a number :") 
+
+  return lines 
+
+# this is the main function 
+def main():
+  balance = deposit()
+  lines = get_number_of_lines()
+  print(balance,lines)
+
+
+
+main()
