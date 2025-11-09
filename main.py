cc7d647446d260e681f4b6aaf4f28f9646dd728b
Gokuldas-dev <gokuldasshenoy2@gmail.com>
2025-11-09 23:31:51 +0530

    Refined the code with return statement properly given

--- main.py
+++ main.py
@@ -1,59 +1,63 @@
-# This is the [PROGRAM FOR Python-Slot-Machine]
-#Global varibale go here --
+# PROGRAM: Python Slot Machine
+
 MAX_LINES = 3
 MAX_BET = 100
 MIN_BET = 1
 
 
-def deposit(): # this is a function to check the amount is digit or not and to make it as a digit (integer)
-  while True:
-    amount = input("what would you like to deposit : ")
-    if amount.isdigit():
-      amount = int(amount)
-      if amount > 0 :
-        break
-      else:
-        print ("The amount must be greater than zero ! ")  # this should be greater than zero 
-    else:
-      print ("Please enter a number :") 
+def deposit():
+    while True:
+        amount = input("What would you like to deposit: ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                return amount
+            else:
+                print("The amount must be greater than zero!")
+        else:
+            print("Please enter a valid number.")
 
-  return amount  #if true the amount will be submited and will be a digit.
 
 def get_number_of_lines():
-  while True:
-    lines = input("Enter the number of lines to bet on  (1-" + str(MAX_LINES) + ")" )
-    if lines.isdigit():
-      lines = int(lines)
-      if 1<= lines <= MAX_LINES:
-        break
-      else:
-        print ("Enter the valid number of lines :  ")  # this should be teh valid range of numbers 
-    else:
-      print ("Please enter a number :") 
+    while True:
+        lines = input(f"Enter the number of lines to bet on (1-{MAX_LINES}): ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                return lines
+            else:
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a valid number.")
 
-  return lines 
 
 def get_bet():
-  while True:
-    bet = input("what would you like to bet : ")
-    if bet.isdigit():
-      bet = int(bet)
-      if MIN_BET <= bet <= MAX_BET:
-        break
-      else:
-        print(f"The Amount must be between {MIN_BET} - {MAX_BET} ")  # this should be between those values  
-    else:
-      print("Please enter a number :") 
+    while True:
+        bet = input("What would you like to bet: ")
+        if bet.isdigit():
+            bet = int(bet)
+            if MIN_BET <= bet <= MAX_BET:
+                return bet
+            else:
+                print(f"The amount must be between {MIN_BET} and {MAX_BET}.")
+        else:
+            print("Please enter a valid number.")
 
-    return bet
 
-# this is the main function 
 def main():
-  balance = deposit()
-  lines = get_number_of_lines()
-  bet = get_bet()
-  print(balance,lines,bet)
- 
+    balance = deposit()
+    lines = get_number_of_lines()
+
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines
+
+        if total_bet > balance:
+            print(f"You don't have enough balance to bet that amount. Your balance is: {balance}")
+        else:
+            break
+
+    print(f"You are betting {bet} on {lines} lines. Total bet is equal to {total_bet}.")
 
 
 main()
